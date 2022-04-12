8026c6b2e5dc619530e3c5e846e0127b5bba2209
visay <vsvang@gmail.com>
2022-04-12 12:28:18 -0500

    April 12, second update

--- js/pwg.js
+++ js/pwg.js
@@ -4,6 +4,7 @@ let password1 = document.querySelector("#password");
 let password2 = document.querySelector("#password2");
 let password3 = document.querySelector("#password3");
 let password4 = document.querySelector("#password4");
+let message = document.querySelector("#message");
 
 //array
 let randomPasswords = ["48b2m2!@#", "2/+3k3$%p50", "564t8w4#$", "9#%$*210!@!"];
@@ -15,4 +16,5 @@ generateBtn.addEventListener("click", () => {
     password3.innerHTML = randomPasswords[2];
     password4.innerHTML = randomPasswords[3];
   }
+  message.innerHTML = "Choose a password";
 });
