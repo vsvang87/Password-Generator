6d1912cb5ead7b4f93729a40d87bc30917cb6efe
vsvang87 <vsvang@gmail.com>
2022-12-04 10:31:17 -0500

    12/04/22, added more options to generate password, added input to html, added styling to input fields, added functions and arrays to javascript

--- js/pwg.js
+++ js/pwg.js
@@ -1,21 +1,166 @@
-const password1 = document.getElementById("password1");
-const password2 = document.getElementById("password2");
-const generateBtn = document.getElementById("generate-btn");
-
 //password character
-const passwordData = [
-  "!dhg87/p0>ds*65",
-  "#j7$d9%o6>2k*u7",
-  "@3l%k6&h4&T8#r4",
-  "&x7&j5$b4@s8!d8",
-  "%sdfa&f9%909asd",
-  "$y9135sdfe&!@d8",
-  "K34ls#$%4@s8!d8",
+const upperCaselettersArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
+const symbolsArray = [
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "|",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+const lowerCaseLettersArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+const password1 = document.getElementById("password1");
+const generateBtn = document.getElementById("generate-btn");
+const characterLength = document.getElementById("character-length");
+const lowercaseEl = document.getElementById("lowercase");
+const uppercaseEL = document.getElementById("uppercase");
+const symbolsEL = document.getElementById("symbols");
+const numberEl = document.getElementById("number");
+
+//storing the random characters
+const randomCharacter = {
+  upper: loopThroughUppercase,
+  lower: loopThroughLowercase,
+  symbol: loopThroughSymbols,
+  number: loopThroughNumber,
+};
+//looping through uppercase letter array
+function loopThroughUppercase() {
+  for (let i = 0; i < upperCaselettersArray.length; i++) {
+    let randomUpperCharacter = Math.floor(
+      Math.random() * upperCaselettersArray.length
+    );
+    return upperCaselettersArray[randomUpperCharacter];
+  }
+}
+//loop through lowercase letter array
+function loopThroughLowercase() {
+  for (let i = 0; i < lowerCaseLettersArray.length; i++) {
+    let randomLowercaseCharacter = Math.floor(
+      Math.random() * lowerCaseLettersArray.length
+    );
+    return lowerCaseLettersArray[randomLowercaseCharacter];
+  }
+}
+//looping through symbols array
+function loopThroughSymbols() {
+  for (let i = 0; i < symbolsArray.length; i++) {
+    let randomSymbolCharacter = Math.floor(Math.random() * symbolsArray.length);
+    return symbolsArray[randomSymbolCharacter];
+  }
+}
+//looping through numbers array
+function loopThroughNumber() {
+  for (let i = 0; i < numbersArray.length; i++) {
+    let randomNumberCharacter = Math.floor(Math.random() * numbersArray.length);
+    return numbersArray[randomNumberCharacter];
+  }
+}
+
+//checking true or false and generate event
 generateBtn.addEventListener("click", () => {
-  let randomPassword1 = Math.floor(Math.random() * passwordData.length);
-  let randomPassword2 = Math.floor(Math.random() * passwordData.length);
-  password1.innerText = passwordData[randomPassword1];
-  password2.innerText = passwordData[randomPassword2];
+  //true or false
+  const characterAmount = +characterLength.value;
+  const includeUppercase = uppercaseEL.checked;
+  const includeLowercase = lowercaseEl.checked;
+  const includeSymbol = symbolsEL.checked;
+  const includeNumber = numberEl.checked;
+
+  password1.innerHTML = generatePassword(
+    characterAmount,
+    includeLowercase,
+    includeUppercase,
+    includeSymbol,
+    includeNumber
+  );
 });
+//generate password
+function generatePassword(length, lower, upper, symbol, number) {
+  //1.initialize password variable
+  let generatedPassword = "";
+  //2.filter out unchecked types
+  let types = lower + upper + symbol + number;
+  let typesArray = [{ upper }, { lower }, { symbol }, { number }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  //if any boxes are checked, if not return 0
+  if (types === 0) {
+    return "password";
+  }
+
+  for (let i = 0; i < length; i += types) {
+    typesArray.forEach((type) => {
+      const randomCharCode = Object.keys(type)[0];
+
+      generatedPassword += randomCharacter[randomCharCode]();
+    });
+  }
+  return generatedPassword;
+}
